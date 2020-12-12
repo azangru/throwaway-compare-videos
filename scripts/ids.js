b7489232f28bf2f373f5ade680192d31af0cba7d
Andrey Azov <andrey@ebi.ac.uk>
2020-12-12 00:04:56 +0000

    Add relevant youtube ids

---
+++ scripts/ids.js
@@ -0,0 +1,11 @@
+export const ids = [
+  ['8MRAIkUtVQw', 'Recorded at: 960x540, saved at: 960x540'],
+  ['pavX-hHX4z0', 'Recorded at: 960x540, saved at: 1440x810'],
+  ['qkwb2DFqf0k', 'Recorded at: 960x540, saved at: 1920x1080'],
+  ['KrSd1p_d8sw', 'Recorded at: 1440x810, saved at: 960x540'],
+  ['Y8lgFQj9Fxk', 'Recorded at: 1440x810, saved at: 1440x810'],
+  ['GFpcZXDkeoo', 'Recorded at: 1440x810, saved at: 1920x1080'],
+  ['VzqmZzgslok', 'Recorded at: 1920x1080, saved at: 960x540'],
+  ['c6c-GFO_Ius', 'Recorded at: 1920x1080, saved at: 1440x810'],
+  ['8zpiTNMy5v0', 'Recorded at: 1920x1080, saved at: 1920x1080']
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,38 @@
 import { html, render } from 'https://unpkg.com/lit-html?module';
 
-const ids = [
-  '3QpfOlm_Gmg',
-  'C2mHnRnBnn0',
-  '0JkcuESmiDM'
-];
+import { ids as youtubeData } from './ids.js';
+
 const width = 950;
 const aspectRatio = 16/9;
 const height = width / aspectRatio;
 
 const renderAll = () => {
-  renderTextarea();
+  // renderTextarea();
   renderVideos();
 };
 
-const renderTextarea = () => {
-  const container = document.querySelector('.controls');
-  const textarea = html`
-    <textarea>${ids.join(' ')}</textarea>
-  `;
+// const renderTextarea = () => {
+//   const container = document.querySelector('.controls');
+//   const textarea = html`
+//     <textarea>${ids.join(' ')}</textarea>
+//   `;
 
-  render(textarea, container);
-}
+//   render(textarea, container);
+// }
 
 const renderVideos = () => {
-  const videos = ids.map(id => renderVideoIframe(id));
+  const videos = youtubeData.map(item => renderVideoIframe(item));
   const container = document.querySelector('.video-list');
 
   render(videos, container);
 };
 
-const renderVideoIframe = (youtubeId) => {
-  const src = `https://www.youtube.com/embed/${youtubeId}`;
+const renderVideoIframe = (data) => {
+  const [id, title] = data;
+  const src = `https://www.youtube.com/embed/${id}`;
   return html`
     <div class="video-wrapper">
+      <h1>${title}</h1>
       <iframe
         src="${src}"
         width="${width}"
